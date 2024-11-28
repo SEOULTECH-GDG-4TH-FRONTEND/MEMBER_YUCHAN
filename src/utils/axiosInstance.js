b142a9a5cd140de376b3ae0c84d126c03b4889d8
letYuchan <yuchancho174@naver.com>
2024-11-28 21:28:07 +0900

    yeah

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "api/", // 서버 주소
+    baseURL: "https://2fe1-203-246-85-188.ngrok-free.app", // 서버 주소
     withCredentials: true, // 쿠키 전송을 위해 설정
 });
 
